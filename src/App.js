1047db333d6865a61586124ebafdfb7db1058834
Noppanat1997 <architon1997@gmail.com>
2020-02-22 02:45:59 +0700

    init-redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
 import Swipe from './components/Swipe.js';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import reducer from './reducer.js';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-function App() {
+
+const App = () => {
+  const store = createStore(reducer, applyMiddleware(logger));
   return (
-    <BrowserRouter className="phone">
-      <Switch>
-        <Route path="/form" component={null}/>
-        <Route path="/swipe" component={null}/>
-        <Route path="/match" component={null}/>
-        <Route path="/chat" component={null}/>
-        <Route exact path="/" component={null}/>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <div className="phone">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/form" component={null} />
+            <Route path="/swipe" component={Swipe} />
+            <Route path="/match" component={null} />
+            <Route path="/chat" component={null} />
+            <Route exact path="/" component={null} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
+
   );
 }
-
 export default App;
