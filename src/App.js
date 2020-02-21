cc8cddd2ddebfba146e01adbb29ccddb3ef8a5d4
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-02-22 02:45:59 +0700

    add register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import Swipe from './components/Swipe.js';
+import Register from './components/Register.jsx';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter className="phone">
+    <div className="phone">
+      <BrowserRouter className="phone">
       <Switch>
-        <Route path="/form" component={null}/>
+        <Route path="/register" component={Register}/>
         <Route path="/swipe" component={null}/>
         <Route path="/match" component={null}/>
         <Route path="/chat" component={null}/>
         <Route exact path="/" component={null}/>
       </Switch>
     </BrowserRouter>
+    </div>
   );
 }
 
