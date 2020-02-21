79ada1cf5a7e4dddd87268386e4a71f79bcb7aaf
Thanyathon <61010497@kmitl.ac.th>
2020-02-22 01:15:26 +0700

    👓 init app with react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/form" component={null}/>
+        <Route path="/swipe" component={null}/>
+        <Route path="/match" component={null}/>
+        <Route path="/chat" component={null}/>
+        <Route exact path="/" component={null}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
