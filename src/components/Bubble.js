b3b152200913e55b767a41d32b9a7957ca1feda5
Thanyathon <61010497@kmitl.ac.th>
2020-02-22 10:46:16 +0700

    🆕 Chat finished v1.0

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Bubble.scss';
 
-export default ({ me }) => {
+export default ({ me, text }) => {
   return (
     <div className={me ? 'bubble-container me' : 'bubble-container'}>
       {!me && <div className="bubble-pic"></div>}
 
       <div className="bubble">
-        <p>สวัสดีครับ ผมเชียร์ลุงมานานแล้ว คุณล่ะเชียร์ยัง?</p>
+        <p>{text}</p>
       </div>
     </div>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.scss';
 import Bubble from './Bubble';
-import { FaArrowLeft, FaEllipsisH } from 'react-icons/fa';
+import { FaArrowLeft, FaEllipsisH, FaTelegramPlane } from 'react-icons/fa';
+
 
 export default () => {
+  const [msg, setMsg] = useState('');
+  const [msgs, setMsgs] = useState([]);
+
+  useEffect(() => {
+    setMsgs([
+      ...msgs,
+      {
+        name: 'Pareena',
+        text: 'สวัสดีครับ ผมเชียร์ลุงมานานแล้ว คุณล่ะเชียร์ยัง?',
+        isMe: false
+      }])
+  }, [])
+
+  useEffect(() => {
+    const ele = document.querySelector('.chat-box');
+    ele.scrollTop = ele.scrollHeight;
+  }, [msgs])
+
+  const sendMsg = e => {
+    e.preventDefault();
+    msgs.push({ name: 'Thanathorn', text: msg, isMe: true });
+    setMsg('');
+    setMsgs([
+      ...msgs,
+      {
+        name: 'Pareena',
+        text: 'สวัสดีครับ ผมเชียร์ลุงมานานแล้ว คุณล่ะเชียร์ยัง?',
+        isMe: false
+      }
+    ])
+  }
+
   return (
     <div className="chat-container">
       <div className="chat-header">
@@ -19,13 +52,20 @@ export default () => {
       </div>
 
       <div className="chat-box">
-        <Bubble/>
-        <Bubble me/>
-        <Bubble me/>
+        {msgs.map(m => <Bubble me={m.isMe} text={m.text}/>)}
+        <div id="lastest-msg"></div>
       </div>
 
       <div className="chat-input">
-        <input placeholder="Type something..."/>
+        <input 
+          id="input-chat"
+          placeholder="Type something..." 
+          value={msg}
+          onChange={e => setMsg(e.target.value)}
+          onKeyPress={e => e.key === 'Enter' ? sendMsg(e) : null}
+          autoFocus
+        />
+        <button onClick={sendMsg}><FaTelegramPlane /></button>
       </div>
 
     </div>
